4e28506fbacc76c6c211e1c387268432a244ea83
emmasarge <emmasargeant206@gmail.com>
2021-06-18 17:28:11 +0000

    fixed bug in navbar

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(existing_user["password"], request.form.get("password")):
+            if check_password_hash(existing_user["password"], request.form.get
+            ("password")):
                 session["user"] = request.form.get("user_name").lower()
                 flash("Welcome, {}".format(
                         request.form.get("user_name")))
@@ -75,7 +76,6 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
-
 
 
 if __name__ == "__main__":
