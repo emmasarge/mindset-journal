9fcf166bde0ae9259f3b13839345ff7f763b5e28
emmasarge <emmasargeant206@gmail.com>
2021-06-18 15:50:19 +0000

    add login

--- app.py
+++ app.py
@@ -33,11 +33,12 @@ def register():
         if existing_user:
             flash(u"Username already exists")
             return redirect(url_for("register"))
-        
+
         register = {	
             "username": request.form.get("username").lower(),	
             "password": generate_password_hash(request.form.get("password"))	
         }
+
         mongo.db.users.insert_one(register)
 
         # Put the new user into 'session' cookie
@@ -46,6 +47,35 @@ def register():
         return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("user_name").lower()
+                flash("Welcome, {}".format(
+                        request.form.get("user_name")))
+                return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 
 
 if __name__ == "__main__":
