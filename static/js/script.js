8ac5e4bda5438797f0819e65fb9ca932686e01a3
emmasarge <emmasargeant206@gmail.com>
2021-07-04 16:36:45 +0000

    minor css change

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,31 @@
 // homepage buttons 
 
-loginBtn.onmouseenter = function(){
+loginBtn.onmouseenter = function () {
     this.classList.add('hovered');
-    }
-    
-    loginBtn.onmouseleave = function(){
-    setTimeout(function(){
-    this.classList.remove('hovered');
-    }.bind(this),2000)
-    }
+}
 
-regBtn.onmouseenter = function(){
+loginBtn.onmouseleave = function () {
+    setTimeout(function () {
+        this.classList.remove('hovered');
+    }.bind(this), 2000)
+}
+
+regBtn.onmouseenter = function () {
     this.classList.add('hovered');
-    }
-        
-    regBtn.onmouseleave = function(){
-    setTimeout(function(){
-    this.classList.remove('hovered');
-    }.bind(this),2000)
-    }
+}
 
+regBtn.onmouseleave = function () {
+    setTimeout(function () {
+        this.classList.remove('hovered');
+    }.bind(this), 2000)
+}
 
+profile.onmouseenter = function () {
+    this.css("background-color: #fafafa");
+}
 
+profile.onmouseleave = function () {
+    setTimeout(function () {
+        this.css("font-family:'Yeseva One', cursive;");
+    }.bind(this), 2000)
+}
